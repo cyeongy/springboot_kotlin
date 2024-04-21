963c8fb0c4ea236d7bfd26ef39d7cbd0a8e21199
cyeongy <cyeongy@gmail.com>
2024-04-21 13:28:07 +0900

    Init Project: Springboot Kotlin

---
+++ src/main/kotlin/com/sample/mysite/MysiteApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.mysite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysiteApplication
+
+fun main(args: Array<String>) {
+    runApplication<MysiteApplication>(*args)
+}
---
+++ src/main/kotlin/com/sample/mysite/user/User.kt
@@ -0,0 +1,4 @@
+package com.sample.mysite.user
+
+class User {
+}
