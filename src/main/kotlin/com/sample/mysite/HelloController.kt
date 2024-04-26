c64b87d5fa7756275a38d989f3732eb7c7a485a0
cyeongy <cyeongy@gmail.com>
2024-04-26 18:43:30 +0900

    Add mustache

--- src/main/kotlin/com/sample/mysite/HelloController.kt
+++ src/main/kotlin/com/sample/mysite/HelloController.kt
@@ -1,14 +1,17 @@
 package com.sample.mysite
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class HelloController {
+
 	@GetMapping("/")
-	fun hello(): String {
-		return "Hello World!"
+	fun index(model: Model): String {
+		return "index"
 	}
 
 	@GetMapping("/{user}")
