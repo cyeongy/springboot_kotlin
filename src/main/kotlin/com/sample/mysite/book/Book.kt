1129578d3fd0194522874605fe79ea936a2dfc9b
cyeongy <cyeongy@gmail.com>
2024-05-02 00:37:00 +0900

    리팩토링

--- src/main/kotlin/com/sample/mysite/book/Book.kt
+++ src/main/kotlin/com/sample/mysite/book/Book.kt
@@ -1,20 +1,20 @@
 package com.sample.mysite.book
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import org.jetbrains.annotations.NotNull
 
 @Entity
 @Table(name = "book")
 class Book(
-	@get:Id
-	@get:GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	var id: Long = 0L,
 
-	@get:NotNull
+	@Column(nullable = false)
 	var title: String,
 
 	var description: String?
--- src/main/kotlin/com/sample/mysite/book/BookService.kt
+++ src/main/kotlin/com/sample/mysite/book/BookService.kt
@@ -1,10 +1,13 @@
 package com.sample.mysite.book
 
+import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(val bookRepository: BookRepository) {
+	private val logger = LoggerFactory.getLogger(BookService::class.java)
+
 	fun save(bookDTO: BookDTO): Long? {
 		return bookRepository.save(bookDTO.toEntity()).id
 	}
